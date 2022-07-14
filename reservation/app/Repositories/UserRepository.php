ba2186dbadc06a58b191bb01e13c8892206d023c
VietVanLe <kiaisoft.viet.le@gmail.com>
2022-07-14 13:33:47 +0700

    xoa function thừa

--- reservation/app/Repositories/UserRepository.php
+++ reservation/app/Repositories/UserRepository.php
@@ -7,17 +7,9 @@ use App\Models\User;
 class UserRepository extends BaseRepository
 {
 
-    protected $msgNotFound = 'Test not found';
-
     public function getModel()
     {
         return User::class;
     }
-
-    public function getAll()
-    {
-        return $this->model->all();
-    }
-
     
 }

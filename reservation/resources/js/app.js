8fd206d29a183792659ed483fe2a93cb65e3498d
= <=>
2022-07-15 10:26:34 +0700

    edit app.js

--- reservation/resources/js/app.js
+++ reservation/resources/js/app.js
@@ -3,15 +3,13 @@ require('./bootstrap');
 window.Vue = require('vue').default;
 
 import '@/plugins'
-import '../css/app.css'
+import '@/assets/css/app.css'
 
 import App from '@/components/App'
 import router from '@/routers/index'
-import store from '@/store'
 
 
 new Vue({
   router,
-  store,
   render: h => h(App),
 }).$mount('#app')

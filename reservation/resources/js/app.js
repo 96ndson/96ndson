9ce789d687b0b619321404cc126ccbebe2fb63ce
TuanNgoc <ngocsival@gmail.com>
2022-07-21 14:11:57 +0700

    homePageFixed

--- reservation/resources/js/app.js
+++ reservation/resources/js/app.js
@@ -5,8 +5,6 @@ import App from '@/components/App';
 import router from '@/routers/index';
 import '@/plugins';
 import '@/assets/css/home.css';
-import $ from "jquery";
-global.$ = $;
 
 new Vue({
   router,
